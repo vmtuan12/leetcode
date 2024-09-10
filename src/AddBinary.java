740c53326f55d60106d9f9fee8659de20ef902be
Vu Minh Tuan <tuanvm@dagoras.io>
2024-09-10 23:29:22 +0700

    add binary

---
+++ src/AddBinary.java
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/add-binary/description/
+
+public class AddBinary {
+    public static void main(String[] args) {
+        String a = "1010";
+        String b = "1011";
+
+        StringBuilder result = new StringBuilder();
+
+        int p1 = a.length() - 1;
+        int p2 = b.length() - 1;
+        int carry = 0;
+
+        while (p1 >= 0 || p2 >= 0) {
+            int c1 = 0;
+            int c2 = 0;
+            if (p1 >= 0) {
+                c1 = a.charAt(p1--) - '0';
+            }
+            if (p2 >= 0) {
+                c2 = b.charAt(p2--) - '0';
+            }
+            int sum = c1 + c2 + carry;
+            result.append(sum % 2);
+            carry = sum / 2;
+        }
+
+        if (carry != 0) {
+            result.append(carry);
+        }
+
+        System.out.println(result.reverse().toString());
+    }
+}
