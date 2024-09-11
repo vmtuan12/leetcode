8e66d6349df4be8b2c6b6a6c596b80089e960f32
Vu Minh Tuan <tuanvm@dagoras.io>
2024-09-11 22:35:24 +0700

    sqrt x

---
+++ src/SqrtX.java
@@ -0,0 +1,23 @@
+public class SqrtX {
+    public static void main(String[] args) {
+        int x = 2147483647;
+
+        if (x < 2) {
+            System.out.println(x);
+        }
+
+        int i = 2;
+        while (i <= (x/2 + 1)) {
+            long pow = (long) i *i;
+            System.out.println(i + " " + pow);
+            if (pow == x) {
+                System.out.println(i);
+                break;
+            } else if (pow > x) {
+                System.out.println(i - 1);
+                break;
+            }
+            i++;
+        }
+    }
+}
