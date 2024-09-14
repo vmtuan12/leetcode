b1d6ba73415dd0649718554cf32e4ec61d150621
Vu Minh Tuan <tuanvm@dagoras.io>
2024-09-14 14:26:43 +0700

    Symmetric Tree

---
+++ src/SymmetricTree.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/symmetric-tree/description/
+
+public class SymmetricTree {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return nodeMatched(root.left, root.right);
+    }
+
+    public boolean nodeMatched(TreeNode p, TreeNode q) {
+        if ((p != null && q == null) || (p == null && q != null)) {
+            return false;
+        }
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return nodeMatched(p.left, q.right) && nodeMatched(p.right, q.left);
+    }
+}
